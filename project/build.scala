3585f2a4cddde9506bf6afeb743da0cc376e5425
Chris <barney.chrisj@gmail.com>
2012-08-28 21:26:33 +1000

    added intellij support

---
+++ project/build.scala
@@ -0,0 +1,10 @@
+import sbt._
+
+object ZuthaBuild extends Build {
+
+  lazy val root = Project(
+    "zutha-redis",
+    file("."))
+
+  // System.setProperty("myproject.root", "E:\\Dropbox\\Projects\\Programming\\Zutha\\zutha-redis")
+}
